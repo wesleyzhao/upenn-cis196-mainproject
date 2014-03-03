d8b2576f89a8169cb0780ec21318703eb6432c2c
Wesley Zhao <wesley.zhao@gmail.com>
2014-03-03 02:00:17 -0500

    removing index in migration

--- db/migrate/20140303040159_add_devise_to_users.rb
+++ db/migrate/20140303040159_add_devise_to_users.rb
@@ -1,8 +1,4 @@
 class AddDeviseToUsers < ActiveRecord::Migration
-  def self.down
-    remove_column :users, :email
-  end
-
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
@@ -41,7 +37,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps
     end
-
+    
+    remove_index :users, :email
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
