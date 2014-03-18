0ffaa6cf55c30c92a98b39302a5c943749b65960
Wesley Zhao <wesley.zhao@gmail.com>
2014-03-17 22:32:04 -0400

    added validations to food item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  if :devise_controller?
-    before_action :configure_devise_permitted_parameters
-  end
+
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
   protected
   
   def configure_devise_permitted_parameters
     registration_params = [:name, :email, :password, :password_confirmation,:height_cm, :weight_kg, :activity_level, :body_fat_percent, :weight_kg_goal, :body_fat_percent_goal, :zipcode, :address_street1, :address_street2, :address_city, :address_state, :address_country]
-
-    if params[:action] == 'update'
+    if params[:action] == "update"
       devise_parameter_sanitizer.for(:account_update) { 
         |u| u.permit(registration_params << :current_password)
       }
-    elsif params[:action] == 'create'
+    elsif params[:action] == "create"
       devise_parameter_sanitizer.for(:sign_up) { 
         |u| u.permit(registration_params) 
       }
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,3 +1,16 @@
 class FoodItem < ActiveRecord::Base
   belongs_to :user
+  
+  validates :name, presence: true
+  
+  # any valid integer
+  nutritionNumValidation = /\A(0)|([1-9]\d*)\z/
+  
+  validates :calories, format: {with: nutritionNumValidation, message: "must be a valid number"}
+
+  validates :protein, format: {with: nutritionNumValidation, message: "must be a valid number"}
+
+  validates :carbs, format: {with: nutritionNumValidation, message: "must be a valid number", :allow_blank => false}
+
+  validates :fat, format: {with: nutritionNumValidation, message: "must be a valid number"}
 end
